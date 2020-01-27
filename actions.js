835558f83f808df3a279cf641ded8acb5b4e14a6
Ashish Singh <46337999+ashishsingh2601@users.noreply.github.com>
2020-01-28 01:27:49 +0530

    js file added

---
+++ actions.js
@@ -0,0 +1,16 @@
+
+var preloader = document.getElementById('loader');
+  function myFunction(){
+    preloader.style.display ='none';
+  }
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+
+  
